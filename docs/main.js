ba42166508c02947bb269c3a2abdaa05989574a9
Pedro Silva <pedro.o.silva975@gmail.com>
2024-11-17 20:12:22 -0300

    fix:slider js

---
+++ docs/main.js
@@ -0,0 +1,59 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 5000);
+  }
+}
+
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  }
+  else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach(slide => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+const form = document.querySelector('.formsContato');
+const inputdoEmail = document.querySelector('.email');
+
+function receberEvento(evento) {
+  evento.preventDefault();
+  if (!inputdoEmail.value) {
+    alert('Por favor, preencha o campo de e-mail.');
+    return;
+  }
+
+  if (!validarEmail(inputdoEmail.value)) {
+    alert('Por favor, insira um e-mail válido. ex: nathan123@gmail.com');
+    return;
+  }
+}
+
+function validarEmail(email) {
+  const regexDoEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regexDoEmail.test(email);
+}
+
+form.addEventListener('submit', receberEvento);
